a48c1153c254de8315df972928342501a905a79e
Jbudry <JoshBudry@Joshs-MacBook-Pro.local>
2016-05-05 14:47:05 -0600

    Google form integrated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('#repair, #sell').mousedown(function(){
-        $('body').empty();
+        $('body').empty().append('<iframe src="https://docs.google.com/forms/d/1S8BjscsUQheDP9CZB-2foYoFx1uk_XLwPUpW1ucYirk/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
     });
 });
