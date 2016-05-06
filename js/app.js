3bc605c2112a6855d17c9991294383ac9911a039
Jbudry <JoshBudry@Joshs-MacBook-Pro.local>
2016-05-05 18:02:11 -0600

    Added Repair Google Page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    $('#repair, #sell').mousedown(function(){
+    $('#sell').mousedown(function(){
         $('body').empty().append('<iframe src="https://docs.google.com/forms/d/1S8BjscsUQheDP9CZB-2foYoFx1uk_XLwPUpW1ucYirk/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
+    });
+    $('#repair').mousedown(function(){
+        $('body').empty().append('<iframe src="https://docs.google.com/forms/d/1nHjvRo1qhdL9HXt453u9SBXUauXrOeQ7KqZXd8ydM3Y/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
     });
 });
