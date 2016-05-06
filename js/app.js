e5beb651b05a9097d27b43681b3032d2e35b075d
Jade Allen Cook <jadeallencook@gmail.com>
2016-05-05 19:58:25 -0600

    ⚗ Some final UI tweakage

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $('.sell').mousedown(function(){
-        $('body').empty().append('<iframe src="https://docs.google.com/forms/d/1S8BjscsUQheDP9CZB-2foYoFx1uk_XLwPUpW1ucYirk/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
+        $('body').empty().append(
+            '<center><a href="index.html"><img src="images/logo.png" style="width: 250px; position:absolute; top: 45px; margin-left: -125px"  /></a></center>' +
+            '<iframe src="https://docs.google.com/forms/d/1S8BjscsUQheDP9CZB-2foYoFx1uk_XLwPUpW1ucYirk/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
+        );
     });
     $('.repair').mousedown(function(){
-        $('body').empty().append('<iframe src="https://docs.google.com/forms/d/1nHjvRo1qhdL9HXt453u9SBXUauXrOeQ7KqZXd8ydM3Y/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
+        $('body').empty().append(
+            '<center><a href="index.html"><img src="images/logo.png" style="width: 250px; position:absolute; top: 45px; margin-left: -125px"  /></a></center>' +
+            '<iframe src="https://docs.google.com/forms/d/1nHjvRo1qhdL9HXt453u9SBXUauXrOeQ7KqZXd8ydM3Y/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
+        );
     });
 });
