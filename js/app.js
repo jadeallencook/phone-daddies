58190638e355b3bd3897dfab1fc08b983c3c2c01
erickwhitford <erickwhitford@gmail.com>
2016-10-17 10:06:18 -0400

    Changed repair form and added comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-$(document).ready(function(){
+/*
+    event listers
+    - sell phone
+    - repair phone 
+*/
+$().ready(function(){
+    // sell phone
     $('.sell').mousedown(function(){
         $('body').empty().append(
             '<center><a href="index.html"><img id="form-logo" src="images/logo.png" /></a></center>' +
             '<iframe src="https://docs.google.com/forms/d/1S8BjscsUQheDP9CZB-2foYoFx1uk_XLwPUpW1ucYirk/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
         );
     });
+    // repair phone
     $('.repair').mousedown(function(){
         $('body').empty().append(
             '<center><a href="index.html"><img id="form-logo" src="images/logo.png" /></a></center>' +
-            '<iframe src="https://docs.google.com/forms/d/1nHjvRo1qhdL9HXt453u9SBXUauXrOeQ7KqZXd8ydM3Y/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
+            '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeCtiPjnmtkCffB364yUjgPmHrSFOCnJKY5DBiilZ-fmIuZgw/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
         );
-    });
+    }); 
 });
