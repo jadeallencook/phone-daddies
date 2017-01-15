5042bddab8d3c84e50558e8505459f38d6bf72f8
Jade Allen Cook <jadeallencook@gmail.com>
2017-01-15 01:40:08 -0500

    ⛏ Added repair section and fixed GitHub issues

--- partials/repair-plugin.php
+++ partials/repair-plugin.php
@@ -10,7 +10,56 @@
         </div>
         <div class="by-brand-product">
             <div class="row active-by-brand slick-arrow-2" id="phone-repairs">
-                
+                <!-- single-brand-product start -->
+                <div class="col-xs-12">
+                    <div class="single-brand-product">
+                        <a href="single-product.html"><img src="img/product/5.jpg" alt=""></a>
+                        <h3 class="brand-title text-gray">
+                            <a href="#">Brand name</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- single-brand-product end -->
+                <!-- single-brand-product start -->
+                <div class="col-xs-12">
+                    <div class="single-brand-product">
+                        <a href="single-product.html"><img src="img/product/6.jpg" alt=""></a>
+                        <h3 class="brand-title text-gray">
+                            <a href="#">Brand name</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- single-brand-product end -->
+                <!-- single-brand-product start -->
+                <div class="col-xs-12">
+                    <div class="single-brand-product">
+                        <a href="single-product.html"><img src="img/product/7.jpg" alt=""></a>
+                        <h3 class="brand-title text-gray">
+                            <a href="#">Brand name</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- single-brand-product end -->
+                <!-- single-brand-product start -->
+                <div class="col-xs-12">
+                    <div class="single-brand-product">
+                        <a href="single-product.html"><img src="img/product/8.jpg" alt=""></a>
+                        <h3 class="brand-title text-gray">
+                            <a href="#">Brand name</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- single-brand-product end -->
+                <!-- single-brand-product start -->
+                <div class="col-xs-12">
+                    <div class="single-brand-product">
+                        <a href="single-product.html"><img src="img/product/8.jpg" alt=""></a>
+                        <h3 class="brand-title text-gray">
+                            <a href="#">Brand name</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- single-brand-product end -->
             </div>
         </div>
     </div>
