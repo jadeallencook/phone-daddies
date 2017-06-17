113166dfa5e6c7b637c8898113a71f4cc470ffc0
Jade Allen Cook <jadeallencook@gmail.com>
2017-06-17 17:43:07 -0400

    🗄 Broke apart theme into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CountDownComponent } from './components/count-down/count-down.component';
+import { ServicesComponent } from './components/services/services.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    BrandsComponent,
+    ProductsComponent,
+    CountDownComponent,
+    ServicesComponent,
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
