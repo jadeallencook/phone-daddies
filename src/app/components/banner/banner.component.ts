2f3b9cd7467fe8a5d23eb779ccee60727b5d8ffe
Jade Allen Cook <jadeallencook@gmail.com>
2017-06-19 11:19:00 -0400

    🔧 Finished homepage setup

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -12,7 +12,7 @@ export class BannerComponent implements OnInit {
       title: "Broken Screen?",
       body: "Throughout our years in business we have developed a staff that can handle the demand of Tawas Michigan and the surrounding area. Our service area now spans to service phones across West branch, Bay City and more!",
       image: "/assets/images/banner/the-shop.jpg",
-      btn: "Repair Phone",
+      btn: "Get It Fixed",
       link: "/repair",
       alt: "blank photo"
     }
